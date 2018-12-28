d0880bb84c3883fe467e2eaaf8cc1e1520048ba3
xing <xghglg@gmail.com>
2018-12-28 16:46:29 +0800

    what

--- src/usual/classes/MainClass.java
+++ src/usual/classes/MainClass.java
@@ -1,9 +1,14 @@
 package usual.classes;
 
+import java.math.BigDecimal;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		BigDecimal num1 = new BigDecimal(1.2);
+		
 
 		System.out.println("Hello World 哦哦哦");
 	}
